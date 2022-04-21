48bbc57188a600bae749f0ced93413c1aaebbb66
MaoScorpion21 <mao7021@hotmail.com>
2022-04-21 00:22:42 -0500

    Debbuging

--- functions_hsh.c
+++ functions_hsh.c
@@ -9,7 +9,9 @@
 #define LSH_RL_BUFSIZE 1024
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
+
 extern char **environ;
+
 /*
  * Function Declarations for builtin shell commands:
  */
@@ -17,6 +19,7 @@ int hsh_cd(char **args);
 int hsh_help(char **args);
 int hsh_exit(char **args);
 int hsh_env(char **args);
+
 /*
  * List of builtin commands, followed by their corresponding functions.
  */
@@ -52,10 +55,10 @@ int hsh_cd(char **args)
 	    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
 	} else
 	{
-	    if (chdir(args[1]) != 0)
-	    {
-		    perror("lsh");
-	    }
+		if (chdir(args[1]) != 0)
+		{
+			perror("lsh");
+		}
 	}
 	return (1);
 
@@ -63,59 +66,64 @@ int hsh_cd(char **args)
 
 int hsh_help(char **args)
 {
-	int i;
+	int i = 0;
 
 	if (args[1] == NULL)
-
-  	printf("simple shell\n");
-  	printf("Type program names and arguments, and hit enter.\n");
-  	printf("The following are built in:\n");
-
-  	for (i = 0; i < hsh_num_builtins(); i++)
 	{
-    		printf("  %s\n", builtin_str[i]);
-  	}
+		printf("simple shell\n");
+		printf("Type program names and arguments, and hit enter.\n");
+		printf("The following are built in:\n");
+	}
+	for (i = 0; i < hsh_num_builtins(); i++)
+	{
+		printf("  %s\n", builtin_str[i]);
+	}
 
-  	printf("Use the man command for information on other programs.\n");
-  	return 1;
+	printf("Use the man command for information on other programs.\n");
+	return (1);
 
 }
 
 int hsh_exit(char **args)
 {
-	if (args[1] == NULL){
+	if (args[1] == NULL)
+	{
 		printf("exit of the shell");
-        }
+	}
 
-
-	return 0;
+	return (0);
 
 }
 
 int hsh_env(char **args)
 {
 	char **s = environ;
-	if (args[1] == NULL){
-        	printf("exit of the shell");
-        }
 
+	if (args[1] == NULL)
+	{
+		printf("exit of the shell");
+	}
+	for (; *s; s++)
+	{
+		printf("%s\n", *s);
+	}
 
-  	for (; *s; s++) {
-    	printf("%s\n", *s);
-  	}
-
-  	return 0;
+	return (0);
 }
 
 char *hsh_read_line(void)
 {
-  char *line = NULL;
-  size_t bufsize = 0; /* have getline allocate a buffer for us */
+	char *line = NULL;
 
-  if (getline(&line,&bufsize, stdin) == -1){
-    if (feof(stdin)) {
+	size_t bufsize = 0; /* have getline allocate a buffer for us */
+
+	if (getline(&line,&bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
       exit(EXIT_SUCCESS);  /* We recieved an EOF*/
-    } else  {
+    } else
+		{
       perror("readline");
       exit(EXIT_FAILURE);
     }
@@ -164,28 +172,28 @@ int hsh_launch(char **args)
 	pid_t pid;
 	int status;
 
-	if (args[0] == NULL) {
-    		/* An empty command was entered.*/
-    		return 1;
+	if (args[0] == NULL)
+	{         /* An empty command was entered.*/
+		return (1);
 	}
 
+	pid = fork();
+	if (pid == 0)
+	{    	/* Child process*/
+		if (execvp(args[0], args) == -1)
+		{
+			perror("lsh");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0)
+	{                   /* Error forking*/
+		perror("lsh");
+	} else
+	{                 /* Parent process*/
 
-  	pid = fork();
-  	if (pid == 0) {
-    	/* Child process*/
-    	if (execvp(args[0], args) == -1) {
-      	perror("lsh");
-    	}
-    	exit(EXIT_FAILURE);
-  	} else if (pid < 0) {
-    	/* Error forking*/
-    	perror("lsh");
-  	} else {
-    	/* Parent process*/
-    	
-      		waitpid(pid, &status, WUNTRACED);
-    	
-  	}
+			waitpid(pid, &status, WUNTRACED);
+
+ 	}
 
 	return (1);
 }
@@ -194,15 +202,17 @@ int hsh_execute(char **args)
 {
 	int i;
 
-  if (args[0] == NULL) {
-    /* An empty command was entered.*/
-    return 1;
-  }
+  if (args[0] == NULL)
+	{                   /* An empty command was entered.*/
+    return (1);
+	}
 
-  for (i = 0; i < hsh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
-      return (*builtin_func[i])(args);
-    }
+  for (i = 0; i < hsh_num_builtins(); i++)
+	{
+		if (strcmp(args[0], builtin_str[i]) == 0)
+			{
+				return (*builtin_func[i])(args);
+			}
   }
 
 	return (hsh_launch(args));
